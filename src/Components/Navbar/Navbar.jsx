361c48e36dd27d7194f8539190cb3fd32c59652d
Umesh <Umesh@umesh.xtha123@gmai.com>
2022-10-16 12:18:06 +0545

    Initial commit

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Topnavbar from '../Topnavbar/Topnavbar'
+
+const Navbar = () => {
+  return (
+   <>
+   <Topnavbar/>
+   </>
+  )
+}
+
+export default Navbar
---
+++ src/Homerouting/Homerouting.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Homerouting = () => {
+  return (
+    <>
+    
+    </>
+  )
+}
+
+export default Homerouting
